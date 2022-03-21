8b793f245e8f5023c1a24bce0a6ccfce7286e7a5
GabrielMatos10 <gabriel_de_pm@hotmail.com>
2022-03-20 23:10:40 -0300

    mudanças desafio complementar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
+type EditTaskArgs = {
+  taskId: number;
+  taskNewTitle: string;
+}
+
+
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const todoWithSameTitle = tasks.find(task => task.title === newTaskTitle)
+
+    if(taskWithSameTitle){
+      return Alert.alert('Task já cadastrada', 'Você não pode cadastrar essa task')
+    }
+
 
     const newTask = {
       id: new Date().getTime(),
@@ -17,7 +31,6 @@ export function Home() {
     }
 
     setTasks(oldTasks =>[...oldTasks, newTask])
-    //TODO - add new task
   }
 
   function handleToggleTaskDone(id: number) {
@@ -34,8 +47,30 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    const updatedTasks = tasks.filter(task => task.id !== id)
+    Alert.alert('Remover item', 'Tem certeza que você deseja remover esse item?', [
+      {
+        style: 'cancel',
+        text: 'Não'
+      },
+      {
+        style: 'destructive',
+        text: 'Sim',
+        onPress: () => {
+          const updatedTasks = task.filter(task=> task.id !== id)
+        }
+      }
+    ])
+  }
 
+  function handleEditTask({taskId, taskNewTitle}:EditTaskArgs) {
+    const updatedTasks = tasks.map(task => ({...task}))
+
+    const taskToBeUpdated = updatedTasks.find(task => task.id === taskId)
+
+    if(!taskToBeUpdated)
+    return
+
+    taskToBeUpdated.title = taskNewTitle
     setTasks(updatedTasks)
   }
 
